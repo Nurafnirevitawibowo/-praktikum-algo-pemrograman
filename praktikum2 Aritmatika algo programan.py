4ae794981e3653e9a97d9b175c99ab424a5f966a
Nurafnirevitawibowo <91048061+Nurafnirevitawibowo@users.noreply.github.com>
2021-09-27 14:48:54 +0700

    Add files via upload

---
+++ praktikum2 Aritmatika algo programan.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+@Materi:Operasi Aritmatika
+@Judul: Praktikum 2 Materi
+@Hari/tanggal: Senin 27/09/2021
+@NIM: 06500210013
+@author: Afni
+"""
+
+#import math
+print("operasi aritmatika")
+a= float(input("nilai a adalah "))
+b= float(input("nilai b adalah "))
+
+print("jumlah a ditambah b adalah:", a+b)
+print("selisih a dan b adalah:", a-b)
+print("jumlah a dikali b adalah:", a*b)
+print("jumlah a dibagi b adalah:", a/b)
+print("jumlah sisa pembagian hasil a dibahi b adalah:", a%b)
+print("hasil a pangkat b adalah:", a**b)
+print("hasil dari log (a)",a)
+
