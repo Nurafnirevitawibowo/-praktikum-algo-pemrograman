deb15e7815d7de2e6bb3b073dd66f29a24b5eb2a
Nurafnirevitawibowo <91048061+Nurafnirevitawibowo@users.noreply.github.com>
2021-09-27 18:49:20 +0700

    Add files via upload

---
+++ praktikum algoritma pemrograman ke2 latihan1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+""" 
+praktikum 2 latihan 1
+created on monday,27 sept 21
+@author: Nurafni Revita Wibowo
+NIm: 065002100013
+"""
+print("Namaku adalah? Nurafni revita wibowo")
+
+
+import math
+print("Operasi Aritmatika")
+n=float(input("nilai n adalah:"))
+y=float(input("nilai y adalah :"))
+
+
+
+print("jumlah n ditambah y adalah:",n+y)
+print("selisih n dan y adalah:",abs(y-n))
+print("jumlah dari n dikali y adalah:",n*y)
+print("jumlah dari n dibagi y adalah:",n/y)
+print("jumlah sisa pembagian dari hasil n dibagi y adalah:",n%y)
+print("hasil log (n)",math.log10(n))
+print("hasil n pangkat y adalah:",n**y)
+
+
